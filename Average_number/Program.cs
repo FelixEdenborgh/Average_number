756d7555d2fde5517eaeb90ec5d807c41b632931
ErrorX <31070311+Nalox1@users.noreply.github.com>
2020-04-26 14:13:16 +0200

    Add files via upload

---
+++ Average_number/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Average_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello this is an average calculator");
+
+            int num1, num2, num3, num4, average;
+
+            Console.WriteLine("Enter first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter secound number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter third number: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter fourd number: ");
+            num4 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("You have enterd the numbers: " + num1 + " " + num2 + " " + num3 + " " + num4);
+
+            average = (num1 + num2 + num3 + num4) / 4;
+            
+
+            Console.WriteLine("The average number of your 4 numbers are: " + average);
+        }
+    }
+}
